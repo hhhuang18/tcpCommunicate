574c79dfc52429813ed1c39ab8ff87afc7035b49
h976567853 <h976567853@gmail.com>
2018-04-28 19:12:00 +0800

    Add files via upload

--- tcpServer.py
+++ tcpServer.py
@@ -1,30 +1,40 @@
-import socket,sys,os,_thread
+import socket,sys,_thread
 
 clients=[]
 
-def handledata(s):
+def recvdata(s):
     global clients
+    flag=[]
     data=s.recv(1024)
     while data:
-        try:
-            for i in clients:
-                # 转发是否包括自己
-                if i != s:
+        for i in clients:
+            # whether send back to self
+            toself=True
+            if (i != s) or toself:
+                try:
                     i.send(data)
-            data=s.recv(1024)
-        except:
-            sys.exit(0)
+                except:
+                    flag.append(i)
+                    continue
+            if len(flag):
+                for i in flag:
+                    print("Disconnected : %s" % str(i.getpeername()))
+                    clients.remove(i)
+                flag=[]
+        data=s.recv(1024)
             
 def main():
+    port=23333  # port
+    cnum=5      # max number of clients
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.bind((socket.gethostname(),2333))# 端口
-    s.listen(5)# 最大连接数
+    s.bind((socket.gethostname(),port))
+    s.listen(cnum)
     print("Waiting For Connections")
     while True:
-        clientsocket,clientaddress=s.accept();
-        clients.append(clientsocket)#新连接加入转发客户端列表
-        _thread.start_new_thread(handledata,(clientsocket,))
-        print("Connected : %s" % str(clientaddress))
+        clientsocket,clientaddress=s.accept()
+        clients.append(clientsocket)# add the new client to the list
+        _thread.start_new_thread(recvdata,(clientsocket,))
+        print("Connected    : %s" % str(clientaddress))
 
 if __name__=="__main__":
     main()
